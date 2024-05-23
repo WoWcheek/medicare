fe03d80e614fab69ca2146f6be932a3c2aa8717c
Savchuk Volodymyr <savchuk.v03@gmail.com>
2024-05-24 00:10:20 +0300

    feat: appointment mockup

--- Medicare.API/RNDVU.WebApp/rndvu.webapp.client/src/pages/Appointment/Appointment.jsx
+++ Medicare.API/RNDVU.WebApp/rndvu.webapp.client/src/pages/Appointment/Appointment.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import de  from 'date-fns/locale/de';
+import { AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import { FormControl,FormLabel, FormControlLabel, Radio, RadioGroup, ListItemButton, ListItemText, List, ListItem, Box } from "@mui/material";
+
 const Appointment = () => {
     const params = useParams()
-
+    const currDate = ()=>{
+      const d=   new Date(Date.now());
+      
+      return new Date(d.toISOString().split('T')[0]);
+    }  
     const history = useNavigate();
 
     if (!params.id) {
         history('/')
     }
+    const shouldDisableDate = date=>{
+        const currentDate = date.toISOString().split("T")[0];
 
+        return date.toDate() < currDate();
+       }
+       const onChange = date=>{
+        return Math.random() > 0.7;
+       }
     // const [doctor, setDoctor] = useState(null);
     // const [getDoctor] = useGetDoctorMutation();
 
@@ -35,16 +47,42 @@ const Appointment = () => {
     //     </div>);
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={de}>
-        <DateCalendar
-        label="Select a date"
-        // value={selectedDate}
-        // onChange={newValue => setSelectedDate(newValue)}
-        // shouldDisableDate={shouldDisableDate}
-        // renderInput={params => <TextField {...params}
-        //  />}
-      />
-        </LocalizationProvider>
+        <div className="d-flex justify-content-between w-100">
+            <div className="m-0">
+            <LocalizationProvider  dateAdapter={AdapterDayjs}>
+                <DateCalendar  shouldDisableDate={shouldDisableDate} onChange={onChange} />
+            </LocalizationProvider>
+            </div>
+            <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">Option</FormLabel>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="male"
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value="male" control={<Radio />} label="Short (30 min)" />
+                    <FormControlLabel value="other" control={<Radio />} label="Long (1 hour)" />
+                </RadioGroup>
+            </FormControl>
+            <Box sx={{ maxWidth: 360, bgcolor: 'background.paper' }}>
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton disablePadding component="a" href="#simple-list">
+                            <ListItemText primary="11:00" />
+                        </ListItemButton>
+                    </ListItem><ListItem disablePadding>
+                        <ListItemButton disablePadding component="a" href="#simple-list">
+                            <ListItemText primary="11:30" />
+                        </ListItemButton></ListItem><ListItem disablePadding>
+                        <ListItemButton disablePadding component="a" href="#simple-list">
+                            <ListItemText primary="12:30" />
+                        </ListItemButton></ListItem><ListItem disablePadding>
+                        <ListItemButton disablePadding component="a" href="#simple-list">
+                            <ListItemText primary="13:00" />
+                        </ListItemButton>
+                    </ListItem>
+                </List></Box>
+</div>
 
     );
 };
